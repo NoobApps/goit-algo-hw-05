73c454052b3f987ae990551d0cb1e49a71290c5c
Vitaliy Ivaskiv <vitalik.ivaskiv88@gmail.com>
2026-09-05 12:13:16 +0300

    t1_mvp

---
+++ task1.py
@@ -0,0 +1,23 @@
+
+def caching_fibonacci():
+    cache={0: 0, 1: 1}
+    def fibonacci(n):
+        if n<=0:
+            return 0
+        if n==1:
+            return 1
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n]=fibonacci(n-1)+fibonacci(n-2)
+            return cache[n]
+        
+    return fibonacci
+
+def main():
+   fib=caching_fibonacci()
+   print(fib(20))
+   
+   
+if __name__=='__main__':
+    main()
