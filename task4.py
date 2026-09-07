83b8cb5a967694e29700d3e5f488f0e8f6aa9626
Vitaliy Ivaskiv <vitalik.ivaskiv88@gmail.com>
2026-09-07 08:29:21 +0300

    mvp

---
+++ task4.py
@@ -0,0 +1,70 @@
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "No such username in dictionary"
+        except IndexError:
+            return "Type username please"
+
+    return wrapper
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        return "Already exists, update if needed"
+    else:
+        contacts[name] = phone
+        return 'Contact added.'
+@input_error
+def change_contact(args, contacts):
+    name, phone= args
+    contacts[name]=phone
+    return "Contact updated"
+    
+@input_error
+def get_phone(args,contacts):
+    name = args[0]
+    return contacts[name]
+    
+
+def get_all(args,contacts):
+    return contacts
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print (contacts)
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command=="change":
+            print(change_contact(args,contacts))
+        elif command=="phone":
+            print(get_phone(args,contacts))
+        elif command=="all":
+            print(get_all(args,contacts))
+
+        else:
+            print("Invalid command.")
+        
+
+if __name__ == "__main__":
+    main()
